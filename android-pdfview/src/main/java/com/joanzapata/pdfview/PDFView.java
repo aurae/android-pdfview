42e8172f4577ebb6b4940a2bf4cf2094747e5ee6
Marcel Schnelle <ms@planetmutlu.com>
2016-05-03 10:07:21 +0200

    Properly call through to onDetachedFromWindow & bumped minSdk to 11 (since PdfView uses API-11 methods anyways)

--- android-pdfview/src/main/java/com/joanzapata/pdfview/PDFView.java
+++ android-pdfview/src/main/java/com/joanzapata/pdfview/PDFView.java
@@ -315,6 +315,7 @@ public class PDFView extends SurfaceView {
     @Override
     protected void onDetachedFromWindow() {
         recycle();
+        super.onDetachedFromWindow();
     }
 
     @Override
